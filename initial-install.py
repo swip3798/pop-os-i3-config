2795389a18691dbb1e2f4352a1c8a56a4e92f16a
Christian <christian.sw@hotmail.de>
2022-01-06 14:44:25 +0100

    Fix docker install

--- initial-install.py
+++ initial-install.py
@@ -28,7 +28,7 @@ _("sudo apt update")
 _("sudo apt upgrade -y")
 
 # Base install
-_("sudo apt install -y i3 lxappearance nitrogen pulseaudio pavucontrol alsa-utils unzip arc-theme compton rofi neofetch arandr snapd curl ssh-askpass-fullscreen alacritty dockerd")
+_("sudo apt install -y i3 lxappearance nitrogen pulseaudio pavucontrol alsa-utils unzip arc-theme compton rofi neofetch arandr snapd curl ssh-askpass-fullscreen alacritty docker.io")
 
 if not args.no_restricted:
     _("sudo apt install -y ubuntu-restricted-extras")
